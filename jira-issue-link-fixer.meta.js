0624c344f2ea27215cfcbe298b91202884f7d0aa
Rory Stokes <contact@rorystok.es>
2019-07-22 10:12:18 +1000

    Fix meta

--- jira-issue-link-fixer.meta.js
+++ jira-issue-link-fixer.meta.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name Jira Issue Link Fixer
 // @namespace http://www.agiledigital.com.au/
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @version      0.3.0
+// @version      0.4.0
 // @description  Issue links in Jira navigate directly to the issue page.
 // @author       Rory Stokes
 // @downloadURL   https://github.com/rorystokes/jira-issue-link-fixer/raw/master/jira-issue-link-fixer.user.js
